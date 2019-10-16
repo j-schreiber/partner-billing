17d30c0ff4ea963537f5d5bf080cddbb1751e658
Jannis Lietzau <info@lietzau-consulting.de>
2019-10-16 17:34:01 +0200

    basic formatting for PDF quick action

--- sfdx-source/packaged/main/core/lwc/invoicePdfQuickAction/invoicePdfQuickAction.js
+++ sfdx-source/packaged/main/core/lwc/invoicePdfQuickAction/invoicePdfQuickAction.js
@@ -1,7 +1,13 @@
 import { LightningElement, api } from 'lwc';
 
+import BUTTON_LABEL_SAVE from '@salesforce/label/c.Button_Label_SaveToAttachments';
+
 export default class InvoicePdfQuickAction extends LightningElement {
     @api invoiceId;
+
+    LABELS = {
+        BUTTON_LABEL_SAVE
+    }
 
     get invoicePdfUrl() {
         return '/apex/InvoicePdf?Id=' + this.invoiceId;
