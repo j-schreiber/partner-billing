095c0e8a683ab873115f1a33bda8d964a3f08af0
Jannis Lietzau <info@lietzau-consulting.de>
2019-10-19 20:36:02 +0200

    invoice pdf generation

--- sfdx-source/packaged/main/core/lwc/invoicePdfQuickAction/invoicePdfQuickAction.js
+++ sfdx-source/packaged/main/core/lwc/invoicePdfQuickAction/invoicePdfQuickAction.js
@@ -1,9 +1,11 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import savePdfToInvoice from '@salesforce/apex/InvoicePdfController.savePdfToInvoice';
 
 import BUTTON_LABEL_SAVE from '@salesforce/label/c.Button_Label_SaveToAttachments';
 
 export default class InvoicePdfQuickAction extends LightningElement {
     @api invoiceId;
+    @track isWorking = false;
 
     LABELS = {
         BUTTON_LABEL_SAVE
@@ -11,5 +13,24 @@ export default class InvoicePdfQuickAction extends LightningElement {
 
     get invoicePdfUrl() {
         return '/apex/InvoicePdf?Id=' + this.invoiceId;
+    }
+
+    savePdf() {
+        this.isWorking = true;
+
+        //console.log('Saving ...');
+        
+        savePdfToInvoice({
+            invoiceId : this.invoiceId
+        })
+        .then( () => {
+            // function body to handle SUCCESS
+            //console.log('Success ...');
+            this.isWorking = false;
+        })
+        .catch( () => {
+            // function body to handle ERRORS
+            this.isWorking = false;
+        })
     }
 }
