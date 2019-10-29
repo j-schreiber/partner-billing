3ec76f9c8e4365606a95bfa1266befa32c64bd63
Jannis Lietzau <info@lietzau-consulting.de>
2019-10-29 22:24:13 +0100

    started ui tests

---
+++ sfdx-source/packaged/invoice-pdf-generation/lwc/invoicePdfQuickAction/__tests__/invoicePdfQuickAction.test.js
@@ -0,0 +1,48 @@
+// lwcComponentName.test.js
+import { createElement } from 'lwc';
+import invoicePdf from 'c/invoicePdfQuickAction';
+
+import savePdfToInvoice from '@salesforce/apex/InvoicePdfController.savePdfToInvoice';
+import getOrganizationProfiles from '@salesforce/apex/InvoicePdfController.getOrganizationProfiles';
+
+// create plain mocks for apex without functionality
+jest.mock(
+    '@salesforce/apex/InvoicePdfController.getOrganizationProfiles',
+    () => {
+        return {
+            default: jest.fn()
+        };
+    },
+    { virtual: true }
+);
+jest.mock(
+    '@salesforce/apex/InvoicePdfController.savePdfToInvoice',
+    () => {
+        return {
+            default: jest.fn()
+        };
+    },
+    { virtual: true }
+);
+
+describe('c-invoice-pdf-quick-action', () => {
+    
+    // reset DOM after each test
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+    
+    /* Helper function to wait until the microtask queue is empty. This is needed for promise
+       timing when calling imperative Apex. */
+    function flushPromises() {
+        // eslint-disable-next-line no-undef
+        return new Promise(resolve => setImmediate(resolve));
+    }
+    
+    test('render pdf quick action: all defaults set', () => {
+      
+    });
+
+});
