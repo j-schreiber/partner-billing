98a4e7f89537b4bf1448a636359df29f47a4f100
Jannis Lietzau <info@lietzau-consulting.de>
2019-11-12 18:07:51 +0100

    fixed data table for invoice card

--- sfdx-source/packaged/invoicing/lwc/invoiceCardRecord/invoiceCardRecord.js
+++ sfdx-source/packaged/invoicing/lwc/invoiceCardRecord/invoiceCardRecord.js
@@ -1,4 +1,5 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
 import { updateRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -9,59 +10,29 @@ import TOAST_TITLE_ERROR from '@salesforce/label/c.Toast_Title_GenericError';
 import TOAST_TITLE_SUCCESS from '@salesforce/label/c.Toast_Title_LineItemsUpdated';
 export default class InvoiceCardRecord extends LightningElement {
     @api recordId;
-    @track hasError = false;
-    @track isWorking = true;
-    @track allLineItems = [];
 
-    dirtyLineItems = new Map();
-    deletedLineItems = new Set();
+    @wire(getInvoice, { recordId : '$recordId' })
+    invoice;
+
+    @track isWorking = true;
 
     LABELS = {
         TOAST_TITLE_ERROR,
         TOAST_TITLE_SUCCESS
     }
 
-    connectedCallback() {
-        this.getLineItemData();
-    }
-
-    removeLineItem(event) {
-
-        let newLineItems = this.allLineItems.filter((value) => {
-            if (value.ExtId !== event.detail) {
-                return true;
-            }
-            if (this.dirtyLineItems.has(value.ExtId)) this.dirtyLineItems.delete(value.ExtId);
-            if (value.Record.Id && value.Record.Id) this.deletedLineItems.add(value.Record.Id);
-            return false;
-        });
-
-        this.allLineItems = newLineItems;
-    }
-
-    updateLineItem(event) {
-        if (!this.dirtyLineItems.has(event.detail.extId)) this.dirtyLineItems.set(event.detail.extId, {});
-        let record = this.dirtyLineItems.get(event.detail.extId);
-        record[event.detail.field] = event.detail.newValue;
-        record.Id = event.detail.recordId;
-    }
-
-    addNewLineItem() {
-        var newItem = this.NewLineItem;
-        this.allLineItems.push(newItem);
-        this.dirtyLineItems.set(newItem.ExtId, newItem.Record);
+    addLineItem() {
+        this.template.querySelector('c-invoice-line-item-datatable').addRow();
     }
 
     commitLineItems() {
         this.isWorking = true;
         commitInvoiceLineItems({
             recordId : this.recordId,
-            lineItemsToUpsert : this.upsertList,
-            lineItemsToDelete : this.deleteList
+            lineItemsToUpsert : this.getModifiedLineItems(),
+            lineItemsToDelete : this.getDeletedLineItems()
         })
         .then( () => {
-            this.dirtyLineItems = new Map();
-            this.deletedLineItems = new Set();
             updateRecord({ fields: { Id: this.recordId } });
             this.dispatchToast('success', TOAST_TITLE_SUCCESS);
             this.isWorking = false;
@@ -73,57 +44,18 @@ export default class InvoiceCardRecord extends LightningElement {
         });
     }
 
-    resetLineItems() {
-        this.getLineItemData();
+    @api
+    reset() {
+        refreshApex(this.wiredInvoice);
+        this.template.querySelector('c-invoice-line-item-datatable').reset();
     }
 
-    get NewLineItem() {
-        return {
-            Record : {
-                Invoice__c : this.recordId,
-                Discount__c : 0.00,
-                Tax__c : 0.00,
-                Quantity__c : 0.00,
-                Price__c : 0.00
-            },
-            ExtId : this.nextLineItemId()
-        };
+    getModifiedLineItems() {
+        return this.template.querySelector('c-invoice-line-item-datatable').getModifiedRows();
     }
 
-    get deleteList() {
-        let arr = [];
-        for (let item of this.deletedLineItems) { arr.push(item); }
-        return arr;
-    }
-
-    get upsertList() {
-        let arr = [];
-        this.dirtyLineItems.forEach( (value) => { arr.push(value); });
-        return arr;
-    }
-
-    incremetor = 0;
-    nextLineItemId() {
-        return this.recordId +'-'+(this.incremetor++);
-    }
-
-    getLineItemData() {
-        this.isWorking = true;
-        getInvoice({
-            recordId : this.recordId
-        })
-        .then((result) => {
-            this.allLineItems = result.LineItems;
-            this.allLineItems.forEach(
-                (item) => { item.ExtId = this.nextLineItemId(); }
-            );
-
-            this.isWorking = false;
-        })
-        .catch(() => {
-            this.hasError = true;
-            this.isWorking = false;
-        })
+    getDeletedLineItems() {
+        return this.template.querySelector('c-invoice-line-item-datatable').getDeletedRows();
     }
 
     dispatchToast(toastVariant, toastTitle, toastMessage) {
