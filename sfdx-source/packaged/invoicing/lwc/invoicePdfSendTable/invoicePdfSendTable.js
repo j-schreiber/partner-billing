ae14952d86236c0d2fb15d4f5922d459dcc379b3
Jannis Lietzau <info@lietzau-consulting.de>
2019-11-19 20:51:20 +0100

    warning box for pdf send table

--- sfdx-source/packaged/invoicing/lwc/invoicePdfSendTable/invoicePdfSendTable.js
+++ sfdx-source/packaged/invoicing/lwc/invoicePdfSendTable/invoicePdfSendTable.js
@@ -4,6 +4,7 @@ import { refreshApex } from '@salesforce/apex';
 import getInvoicesWithPdfs from '@salesforce/apex/BillingController.getInvoicesWithPdfs';
 
 import CARD_TITLE from '@salesforce/label/c.Invoicing_Label_InvoicesPdfSendTitle';
+import MESSAGE_NO_RECORDS from '@salesforce/label/c.Message_Invoicing_NoPdfSendInvoices';
 
 export default class UnvoicePdfSendTable extends LightningElement {
 
@@ -13,11 +14,16 @@ export default class UnvoicePdfSendTable extends LightningElement {
     @track isWorking = false;
 
     LABELS = {
-        CARD_TITLE
+        CARD_TITLE,
+        MESSAGE_NO_RECORDS
     }
 
     refreshData() {
         refreshApex(this.invoices);
+    }
+
+    get hasNoRecords() {
+        return (this.invoices.data && this.invoices.data.length === 0);
     }
 
 }
