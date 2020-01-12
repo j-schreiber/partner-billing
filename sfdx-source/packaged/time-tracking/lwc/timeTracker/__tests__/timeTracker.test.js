1ed0fbb507c09c8fe4f1f37f30301734e193b003
Jannis Lietzau <info@lietzau-consulting.de>
2020-01-12 19:45:24 +0100

    translations and some more tests

--- sfdx-source/packaged/time-tracking/lwc/timeTracker/__tests__/timeTracker.test.js
+++ sfdx-source/packaged/time-tracking/lwc/timeTracker/__tests__/timeTracker.test.js
@@ -1,5 +1,10 @@
 import { createElement } from 'lwc';
 import timeTrackerWidget from 'c/timeTracker';
+import {
+    getRecordCreateDefaults,
+    getRecord
+} from 'lightning/uiRecordApi';
+import { registerLdsTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
 
 import getUnfinishedTimeEntries from '@salesforce/apex/TimeTrackingController.getUnfinishedTimeEntries';
 
@@ -9,8 +14,12 @@ jest.mock(
     { virtual: true }
 );
 
+const getRecordWireAdapter = registerLdsTestWireAdapter(getRecord);
+const getRecordCreateDefaultsWireAdapter = registerLdsTestWireAdapter(getRecordCreateDefaults);
+
 const EMPTY_LIST = require('./data/empty-list.json');
 const OPEN_TIME_ENTRIES = require('./data/open-time-entries.json');
+const EXISTING_TIME_ENTRY = require('./data/existing-record.json');
 
 describe('c-time-tracker', () => {
 
@@ -34,6 +43,9 @@ describe('c-time-tracker', () => {
 
             let accountInput = element.shadowRoot.querySelector('lightning-input-field[data-id="InputAccount__c"]');
             expect(accountInput.disabled).toBe(false);
+
+            let startRecordingButton = element.shadowRoot.querySelector('lightning-button-icon[data-id="startRecordingButton"]');
+            expect(startRecordingButton).not.toBeNull();
             
         });
 
@@ -54,6 +66,33 @@ describe('c-time-tracker', () => {
             let accountInput = element.shadowRoot.querySelector('lightning-input-field[data-id="InputAccount__c"]');
             expect(accountInput.disabled).toBe(true);
             
+        });
+
+        test('open time entry fully loaded => full functionality available for time entry', async () => {
+
+            getUnfinishedTimeEntries.mockResolvedValue(OPEN_TIME_ENTRIES);
+        
+            const element = createElement('c-time-tracker', {
+                is: timeTrackerWidget 
+            });
+            document.body.appendChild(element);
+            await Promise.resolve();
+
+            getRecordWireAdapter.emit(EXISTING_TIME_ENTRY);
+            await Promise.resolve();
+
+            let dailyRateInput = element.shadowRoot.querySelector('lightning-input[data-id="InputDailyRate__c"]');
+            expect(dailyRateInput.value).toBe(EXISTING_TIME_ENTRY.fields.DailyRate__c.value);
+
+            let descriptionInput = element.shadowRoot.querySelector('lightning-textarea[data-id="InputDescription__c"]');
+            expect(descriptionInput.value).toBe(EXISTING_TIME_ENTRY.fields.Description__c.value);
+
+            let submitButton = element.shadowRoot.querySelector('lightning-button[data-id="submitTimeEntryButton"]');
+            expect(submitButton).not.toBeNull();
+
+            let deleteButton = element.shadowRoot.querySelector('lightning-button-icon[data-id="deleteTimeEntryButton"]');
+            expect(deleteButton).not.toBeNull();
+
         });
         
     });
