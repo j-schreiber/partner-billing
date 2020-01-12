0f3567659d90aca007622eb90c5df2d72e649308
Jannis Lietzau <info@lietzau-consulting.de>
2020-01-12 18:32:36 +0100

    removed unused fields; added some tests

--- sfdx-source/packaged/time-tracking/lwc/timeTracker/__tests__/timeTracker.test.js
+++ sfdx-source/packaged/time-tracking/lwc/timeTracker/__tests__/timeTracker.test.js
@@ -5,26 +5,54 @@ import getUnfinishedTimeEntries from '@salesforce/apex/TimeTrackingController.ge
 
 jest.mock(
     '@salesforce/apex/TimeTrackingController.getUnfinishedTimeEntries',
-    () => {
-        return {
-            default: jest.fn()
-        };
-    },
+    () => { return { default: jest.fn() }; },
     { virtual: true }
 );
 
+const EMPTY_LIST = require('./data/empty-list.json');
+const OPEN_TIME_ENTRIES = require('./data/open-time-entries.json');
+
 describe('c-time-tracker', () => {
 
-    describe('get wired data', () => {
+    describe('initialization', () => {
     
         afterEach(() => { reset(); });
-        
-        test('test 1', () => {
+
+        test('no open time entries => starts blank', async () => {
+
+            getUnfinishedTimeEntries.mockResolvedValue(EMPTY_LIST);
         
             const element = createElement('c-time-tracker', {
                 is: timeTrackerWidget 
             });
             document.body.appendChild(element);
+
+            await Promise.resolve();
+
+            let clock = element.shadowRoot.querySelector('c-duration-timer');
+            expect(clock).toBeNull();
+
+            let accountInput = element.shadowRoot.querySelector('lightning-input-field[data-id="InputAccount__c"]');
+            expect(accountInput.disabled).toBe(false);
+            
+        });
+
+        test('has open time entries => continues existing time entry', async () => {
+
+            getUnfinishedTimeEntries.mockResolvedValue(OPEN_TIME_ENTRIES);
+        
+            const element = createElement('c-time-tracker', {
+                is: timeTrackerWidget 
+            });
+            document.body.appendChild(element);
+
+            await Promise.resolve();
+
+            let clock = element.shadowRoot.querySelector('c-duration-timer');
+            expect(clock).not.toBeNull();
+
+            let accountInput = element.shadowRoot.querySelector('lightning-input-field[data-id="InputAccount__c"]');
+            expect(accountInput.disabled).toBe(true);
             
         });
         
